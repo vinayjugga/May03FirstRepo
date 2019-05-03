3a24d438d0abd30691a77cb653a522968dfbdac4
Vinay <mlvinay@gmail.com>
2019-05-03 14:52:56 +0530

    My first commit

---
+++ src/com/vinay/synechron/collections/ArrayListExample.java
@@ -0,0 +1,21 @@
+package com.vinay.synechron.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		
+		List<String> a1 = new ArrayList<String>() ;
+		
+		a1.add("vinay") ;
+		a1.add("Ravi") ;
+		a1.add("raju") ;
+		
+		System.out.println(a1);
+	
+		
+	}
+
+}
