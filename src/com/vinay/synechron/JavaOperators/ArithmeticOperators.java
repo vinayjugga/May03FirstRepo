22092837a3dc077631a3b1087ecace8b147c030e
VINAY.l <VINAY.l@SYNBLRDT5DMWQG2.synechron.com>
2019-05-20 17:54:51 +0530

    Added files

---
+++ src/com/vinay/synechron/JavaOperators/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package com.vinay.synechron.JavaOperators;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+
+		int a = 10 ;
+		int b = 20 ;
+		
+		System.out.println("Addition operator: ");
+
+	}
+
+}
