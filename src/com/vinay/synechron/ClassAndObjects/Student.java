ef58acb7bc80721827022f464173059f30ca685f
vinayjugga <vinayjugga@vinay-jugga>
2019-05-26 22:13:03 +0530

    class and objects

--- src/com/vinay/synechron/ClassAndObjects/Student.java
+++ src/com/vinay/synechron/ClassAndObjects/Student.java
@@ -3,10 +3,12 @@ package com.vinay.synechron.ClassAndObjects;
 public class Student {
 	
 	String name ="Vinay" ; //global variable 
+	int age =35 ;
 	
 	public void display() //method 
 	{
 		System.out.println("Name displayed: "+name);
+		System.out.println("Age : "+ age);
 	}
 
 	public static void main(String[] args) {
