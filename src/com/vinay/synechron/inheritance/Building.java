3b2dfb6a68256be8c177590951d84ddc9f7ee27d
User <User@SYNBLRDT647XZ9M>
2019-05-30 15:02:21 +0530

    inheritance file

---
+++ src/com/vinay/synechron/inheritance/Building.java
@@ -0,0 +1,10 @@
+package com.vinay.synechron.inheritance;
+
+public interface Building {
+
+	public void floor1() ;
+	public void floor2() ;
+	public void floor3() ;
+	public void floor4() ;
+	public void floor5() ;
+}
---
+++ src/com/vinay/synechron/selenium/topics/WebDriver.java
@@ -0,0 +1,20 @@
+package com.vinay.synechron.selenium.topics;
+
+public interface WebDriver {
+		
+	public abstract void verifyMethod() ;	
+	public void get() ;
+	public void findElements() ;
+	public void back() ;
+	public void getCurrentURL() ;
+	/*public void getTitile()
+	{
+		System.out.println("title print");
+	}*/
+	
+	
+	
+}
+
+
+
