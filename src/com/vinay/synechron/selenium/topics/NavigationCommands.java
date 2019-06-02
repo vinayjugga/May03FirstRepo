cb09c31982464f9afe9ee1a2492cffef3f1ead0f
vinayjugga <vinayjugga@vinay-jugga>
2019-06-02 11:35:43 +0530

    comands

--- src/com/vinay/synechron/selenium/topics/NavigationCommands.java
+++ src/com/vinay/synechron/selenium/topics/NavigationCommands.java
@@ -5,16 +5,10 @@ public class NavigationCommands extends BasePage {
 	public static void main(String[] args) {
 		
 		BasePage.openPage("http:\\www.google.com");
-		driver.navigate().to("http:\\www.bing.com");
-		driver.navigate().to("http:\\www.gmail.com");
+		driver.navigate().to("http:\\www.bing.com");	
 		driver.navigate().back();
-		String utl = driver.getCurrentUrl();
-		if(utl.contains("bing")){
-			System.out.println(utl);
-			System.out.printf("Back page %s loaded successfully",utl);
-		}else{
-			System.out.println("bing page is not loaded");
-		}
+		driver.navigate().forward();
+		driver.navigate().refresh();		
 		driver.close();
 
 	}
