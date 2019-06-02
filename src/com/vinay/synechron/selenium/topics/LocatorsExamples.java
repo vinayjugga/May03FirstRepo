e839e59db81c4e0e2e0371f157bcd3b265300b92
vinayjugga <vinayjugga@vinay-jugga>
2019-06-02 11:24:10 +0530

    locator search box

--- src/com/vinay/synechron/selenium/topics/LocatorsExamples.java
+++ src/com/vinay/synechron/selenium/topics/LocatorsExamples.java
@@ -1,5 +1,10 @@
 package com.vinay.synechron.selenium.topics;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public class LocatorsExamples extends BasePage{
 
 	public static void main(String[] args) {
@@ -7,7 +12,14 @@ public class LocatorsExamples extends BasePage{
 		BasePage.openPage("https://www.google.co.in/");		
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());	
-		//System.out.println(driver.getPageSource());
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("selenium");	
+		
+		List<WebElement> prepoplatedSearch = driver.findElements(By.xpath("//ul")) ;		
+		for(WebElement el: prepoplatedSearch)
+		{
+			System.out.println(el.getText());
+		}
 		
 
 	}
